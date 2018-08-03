e48bcb3797ec162032d21a7050bbae901619220c
bchang <bchang@fulcrm.com>
2018-08-03 18:15:59 +0800

    update files

--- src/App.vue
+++ src/App.vue
@@ -20,19 +20,20 @@
             <svg aria-hidden="true" data-prefix="fab" data-icon="youtube" class="svg-inline--fa fa-youtube fa-w-18" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="currentColor" d="M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597-11.412 42.867-11.412 132.305-11.412 132.305s0 89.438 11.412 132.305c6.281 23.65 24.787 41.5 48.284 47.821C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.321 42.003-24.171 48.284-47.821 11.412-42.867 11.412-132.305 11.412-132.305s0-89.438-11.412-132.305zm-317.51 213.508V175.185l142.739 81.205-142.739 81.201z"></path></svg>
           </span>
           </a>
-          <div id="navbarBurger" class="navbar-burger burger" data-target="navMenuDocumentation" style="height: auto;">
+
+          <div id="navbarBurger" class="navbar-burger burger" data-target="navMenuDocumentation" style="height: auto;" v-bind:class="[isActive ? 'is-active' : '']" v-on:click="toggleClass">
             <span></span>
             <span></span>
             <span></span>
           </div>
         </div>
 
-        <div id="navMenuDocumentation" class="navbar-menu">
+        <div id="navMenuDocumentation" class="navbar-menu" v-bind:class="[isActive ? 'is-active' : '']">
           <div class="navbar-start">
-            <router-link to="/show" class="navbar-item bd-navbar-item-videos">藝富演藝</router-link> |
+            <router-link to="/show" class="navbar-item bd-navbar-item-videos">藝富演藝</router-link>
             <router-link to="/space" class="navbar-item bd-navbar-item-blog">藝富空間</router-link>
 
-            <div class="navbar-item has-dropdown is-hoverable">
+            <!--<div class="navbar-item has-dropdown is-hoverable">
 
               <a class="navbar-link" href="#">
                 更多
@@ -90,7 +91,7 @@
                 </a>
 
               </div>
-            </div>
+            </div>-->
           </div>
 
           <div class="navbar-end">
@@ -145,6 +146,21 @@
     </footer>
   </div>
 </template>
+
+<script>
+  export default {
+    data: function() {
+      return {
+        isActive: false
+      }
+    },
+    methods: {
+      toggleClass() {
+        this.isActive = !this.isActive;
+      }
+    }
+  }
+</script>
 
 <style>
   .footer-title {
